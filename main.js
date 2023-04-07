4328972ba92316276fe9d586ad5d2fbc8a54a9cf
omerbak <42501059+omerbak@users.noreply.github.com>
2023-04-07 05:40:41 +0300

    add the edit task basic fonctionality

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let input = document.querySelector(".input");
 let add = document.querySelector(".add");
 let tasksDiv = document.querySelector(".tasks");
-let form = document.querySelector(".form");
+let addForm = document.querySelector(".add-form");
+let editForm = document.querySelector(".edit-form");
+let editPage = document.querySelector(".edit-page")
+let cancelEditForm = document.querySelector(".cancel-btn")
 //Empty arry of task
 let arrayTasks = [];
-
+//task id for the task to edit
+let taskId;
+let taskToEdit;
 //check if there is tasks in local storage
 if(localStorage.getItem("tasks")){
     arrayTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -15,32 +20,73 @@ if(localStorage.getItem("tasks")){
 
 
 // add task to array when the form is submitted
-form.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let inputText = input.value;
     if(inputText !== ""){    //add task if in input is not empty 
         addTaskToArray(inputText.slice(0, 1).toUpperCase()+inputText.slice(1));
         input.value = "";
-    
     }
 })
+// edit task when the edit form is submitted
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let editFormText = editForm.text.value;
+    editTasksArray(taskId, editFormText)
+    editPage.classList.remove("active")
+    console.log(editForm.text.value)
+    console.log(taskId)
+})
+// Delete task fonctionality
+/* deleteOptions.forEach(item => {
+    item.addEventListener('click', (e) => {
+        item.parentElement.remove()
+    })
+}) */
 
-//Click on task element (delete and commpleted task fonctionality)
+//Click on task element (delete, edit and commpleted task fonctionality)
 tasksDiv.addEventListener("click", (e) => {
-    //Delete  button
-    if(e.target.classList.contains("delete")){
+    //Delete  task fonctionality
+    if(e.target.classList.contains("delete-option")){
         //Remove task from local storage
-        deleteTask(e.target.parentElement.getAttribute("data-id"));
+        deleteTask(e.target.parentElement.parentElement.getAttribute("data-id"));
         //Remove element from page
-        e.target.parentElement.remove();
-    } else if(e.target.parentElement.classList.contains("delete")) {
+        e.target.parentElement.parentElement.remove();
+    } else if(e.target.parentElement.classList.contains("delete-option")) {
          //Remove task from local storage
-         deleteTask(e.target.parentElement.parentElement.getAttribute("data-id"));
+         deleteTask(e.target.parentElement.parentElement.parentElement.getAttribute("data-id"));
          //Remove element from page
-         e.target.parentElement.parentElement.remove();
+         e.target.parentElement.parentElement.parentElement.remove();
     }
 
-    //Task element
+    // edit fonctionality
+    if(e.target.classList.contains("edit-option")){
+        editPage.classList.add("active")
+        //Remove task from local storage
+        taskId = e.target.parentElement.parentElement.getAttribute("data-id");
+        taskToEdit = arrayTasks.find((task) =>  taskId == task.id)
+        console.log("task id",taskId)
+        console.log("tasks array",arrayTasks)
+        console.log("task to edit", taskToEdit)
+        editForm.text.value = taskToEdit.title
+       //Remove element from page
+       // e.target.parentElement.parentElement.remove();
+       
+    } else if(e.target.parentElement.classList.contains("edit-option")) {
+        editPage.classList.add("active")
+         //Remove task from local storage
+         taskId = e.target.parentElement.parentElement.parentElement.getAttribute("data-id");
+         taskToEdit = arrayTasks.find((task) =>  taskId == task.id)
+        console.log("task id",taskId)
+        console.log("tasks array",arrayTasks)
+        console.log("task to edit", taskToEdit)
+        editForm.text.value = taskToEdit.title;
+         //Remove element from page
+         //e.target.parentElement.parentElement.parentElement.remove();
+        
+    }
+
+    //check Task element
     if(e.target.classList.contains("task") ){
         //toggle completed for the task
         toggleStatusTask(e.target.getAttribute("data-id"));
@@ -74,6 +120,19 @@ function addTaskToArray(taskText){
     addTolacalStorage(arrayTasks);
 }
 
+function editTasksArray(taskId, taskText){
+    console.log("editTasksArray function")
+    arrayTasks = arrayTasks.map((item) => {
+        if(item.id == taskId){
+            return {...item, title: taskText}
+        } else {
+            return item;
+        }
+    })
+    addTolacalStorage(arrayTasks);
+    addElementsToPageFrom(arrayTasks)
+}
+
 function addElementsToPageFrom(arrayTasks){
     //Empty tasksDiv
     tasksDiv.innerHTML = "";
@@ -97,8 +156,15 @@ function addElementsToPageFrom(arrayTasks){
 
         //Create and append Delete button 
         let span = document.createElement("div");
-        span.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-        span.className = "delete";
+        let editOption = document.createElement("div");
+        editOption.classList.add("edit-option")
+        let deleteOption = document.createElement("div");
+        deleteOption.classList.add("delete-option");
+        deleteOption.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+        editOption.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+        span.className = "options-box";
+        span.appendChild(editOption);
+        span.appendChild(deleteOption);
         div.appendChild(span);
 
         //Add task to taskDiv(page)
@@ -170,7 +236,10 @@ function filterTasks(filter){
     console.log(arrayTasks); */
 }
 
-
+// close edit form
+cancelEditForm.addEventListener("click", () => {
+    editPage.classList.remove("active")
+})
 
 
 /* let localTasks = [];
